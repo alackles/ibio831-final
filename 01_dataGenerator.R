02c341763f18e79a51c0170bfc1b8b5ffceb4e70
Acacia Ackles <alackles@msu.edu>
2021-02-24 13:33:10 -0500

    Edit data generator to show interaction of A and B

--- 01_dataGenerator.R
+++ 01_dataGenerator.R
@@ -6,13 +6,12 @@
 
 #### Number of Datapoints ####
 # Assume a balanced design
-# 4 Genotypes - WT, A, B, AB
+# 2 kinds of genes - A and B
 # Arbitrary number of replicates performed
-n.types <- 4
 n.reps <- 50
 
 #Total number of data points
-n <- n.types * n.reps
+n <- n.types*n.reps
 
 # For a binomial distribution, we need to know the number of organisms in each replicate
 # In our work we usually use the same number of organsims in each replicate
@@ -20,9 +19,10 @@ n.orgs <- rep(100,n)
 
 
 #### Data Simulation ####
-# Specify a factor variable to indicate genotype
-type.names <- c("WT", "A", "B", "AB")
-type <- factor(rep(type.names, each=n.reps), levels=type.names) # levels here ensures WT is the reference var
+
+# Specify a categorical variable which indicates genotype
+genA <- factor(rep(c(0,1), each=n/2)) # produces 0000...1111....
+genB <- factor(rep(rep(c(0,1), each=n/4),2)) # produces 00...11...00...11...
 
 # We also need a vector to indicate mass for each organism
 # Assume this is a continuous variable between 1 and 10 grams
@@ -31,8 +31,8 @@ mass <- round(runif(n, min=1, max=10), digits=2)
 # Chose the values for the parameters (logit transformed)
 # Labeled to make it easier to think about
 # Each element in beta.vec.names indicates the effect of that variable or interaction of variables
-beta.vec.names <- c("WT", "A", "B", "AB", "WT:mass", "A:mass", "B:mass", "AB:mass")
-beta.vec <- c(-0.4, 0.1, 0.2, 0.2, 0.6, 0.2, -0.5, 0.9) # we can freely edit these
+beta.vec.names <- c("genA", "genB", "genA:genB", "mass")
+beta.vec <- c(-0.4, 0.1, 0.2, 0.2) # we can freely edit these
 names(beta.vec) <- beta.vec.names
 
 
